542e3950145a9c1ff80dd95d1f89fe6aa5e1d89f
Дианка Коренько:) <114017274+DiasikKorenko@users.noreply.github.com>
2023-05-25 20:45:43 +0300

    update

--- src/main/java/com/tms/Main.java
+++ src/main/java/com/tms/Main.java
@@ -3,12 +3,9 @@ package com.tms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-
     }
 }
-
